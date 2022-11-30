b9cad6baba794d636f835f199b3d73416660bef0
turn-sole <113235914+turn-sole@users.noreply.github.com>
2022-11-30 22:30:13 +0800

    Add files via upload

---
+++ hkj.c
@@ -0,0 +1,79 @@
+//typedef struct stu
+//{
+//	int a;
+//	char b[10];
+//	int* p;
+//}stu;
+//int main()
+//{
+//	struct stu s;
+//	strcpy(s.b, "abcde");
+//	s.a = 10;
+//	return 0;
+//}
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//};
+//void print(struct stu* ps)
+//{
+//	printf("name=%s   age=%d\n", ps->name, ps->age);
+//}
+//int main()
+//{
+//	struct stu s = { "zhangsan",20 };
+//	print(&s);
+//	return 0;
+//}
+//struct stu
+//{
+//	int data[1000];
+//	int num;
+//};
+//struct stu s = { {1,2,3,4},1000 };
+//void print1(struct stu s)
+//{
+//	printf("%d\n", s.num);
+//}
+//void print2(struct stu* ps)
+//{
+//	printf("%d\n", ps->num);
+//}
+//int main()
+//{
+//	print1(s);
+//	print2(&s);
+//	return 0;
+//}
+#include<stdio.h>
+//struct S
+//{
+//	char arr[10];
+//	int age;
+//	float f;
+//};
+//int main()
+//{
+//	struct S s = { "hello",20,5.5f };
+//	struct S tmp = { 0 };
+//	char buf[100] = { 0 };
+//	sprintf(buf, "%s %d %f", s.arr, s.age, s.f);
+//	printf("%s\n", buf);
+//	sscanf(buf, "%s %d %f", tmp.arr, &(tmp.age), &(tmp.f));
+//	printf("%s %d %f\n", tmp.arr, tmp.age, tmp.f);
+//	return 0;
+//}
+//int main()
+//{
+//	FILE* pf = { "test.txt","r" };
+//	if (pf == NULL)
+//	{
+//
+//	}
+//}
+#include<stdio.h>
+int main()
+{
+
+}
